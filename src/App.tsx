1053da4efbdc95e15269dffbaed5fa5a990f3aa9
Zvonimir Milicevic <zvonimir.milicevic112@gmail.com>
2023-12-07 10:05:21 +0100

    initial commit - layout - pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
+
+import Layout from './Layout/Layout';
+import PostPage from './pages/PostPage';
+import PostsPage from './pages/PostsPage';
+
+const App = () => {
+	return (
+		<Routes>
+			<Route
+				element={
+					<Layout>
+						<Outlet />
+					</Layout>
+				}
+			>
+				<Route path="posts" element={<PostsPage />} />
+				<Route path="post/:postId" element={<PostPage />} />
+				<Route path="*" element={<Navigate to="posts" replace />} />
+			</Route>
+		</Routes>
+	);
+};
 
 export default App;
---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+type Props = {
+	children: React.ReactNode;
+};
+
+const Layout = ({ children }: Props) => {
+	return (
+		<div className="app-layout">
+			<header>
+				<div className="app-width-container">React Developer Assignment</div>
+			</header>
+			<main className="app-content">{children}</main>
+			<footer></footer>
+		</div>
+	);
+};
+
+export default Layout;
